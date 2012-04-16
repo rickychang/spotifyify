86659e38777fc7e4f0f59128ada066998311cdae
Ricky Chang <rchang1@gmail.com>
2012-04-15 23:35:08 -0400

    first commit

---
+++ spotifyify.js
@@ -0,0 +1,33 @@
+function spotifyifySelection() {
+    if (typeof window.getSelection != "undefined") {
+        var sel = window.getSelection();
+        if (sel.rangeCount) {
+            var searchQuery = sel.toString();
+            var range = sel.getRangeAt(0);
+            var newNode = document.createElement('a');
+            var spotifyApiUrl = 'http://ws.spotify.com/search/1/album.json?q=' + encodeURI(searchQuery);
+            var spotifyRequest = new XMLHttpRequest();
+            var albumURI = "";
+            spotifyRequest.onreadystatechange= function() {
+                if (spotifyRequest.readyState==4) {
+                    if (spotifyRequest.status==200 || window.location.href.indexOf("http")==-1) {
+                        var spotifyAPIResponse = JSON.parse(spotifyRequest.response);
+                        try {
+                            albumURI = spotifyAPIResponse['albums'][0]['href'];
+                            console.log(spotifyAPIResponse);
+                            newNode.href = albumURI;
+                            range.surroundContents(newNode);
+                            location.href = albumURI;
+                        }
+                        catch (err) {
+                            return;
+                        }
+                    }
+                }   
+            }
+            spotifyRequest.open("GET", spotifyApiUrl, true);
+            spotifyRequest.send(null);
+        }
+    } 
+};
+spotifyifySelection();
