10899e57bd1e3e60edd094e0b487be8a1f2bb895
Ricky Chang <ricky.m.chang@gmail.com>
2012-04-16 16:27:53 -0400

    removing console.log statement

--- spotifyify.js
+++ spotifyify.js
@@ -3,18 +3,17 @@ function spotifyifySelection() {
         var sel = window.getSelection();
         if (sel.rangeCount) {
             var searchQuery = sel.toString();
-            var range = sel.getRangeAt(0);
-            var newNode = document.createElement('a');
             var spotifyApiUrl = 'http://ws.spotify.com/search/1/album.json?q=' + encodeURI(searchQuery);
             var spotifyRequest = new XMLHttpRequest();
             var albumURI = "";
+            var range = sel.getRangeAt(0);
+            var newNode = document.createElement('a');
             spotifyRequest.onreadystatechange= function() {
                 if (spotifyRequest.readyState==4) {
                     if (spotifyRequest.status==200 || window.location.href.indexOf("http")==-1) {
                         var spotifyAPIResponse = JSON.parse(spotifyRequest.response);
                         try {
                             albumURI = spotifyAPIResponse['albums'][0]['href'];
-                            console.log(spotifyAPIResponse);
                             newNode.href = albumURI;
                             range.surroundContents(newNode);
                             location.href = albumURI;
